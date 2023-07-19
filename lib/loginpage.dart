1e64e7e66d5114a5af0c281982d10c12307673af
RajPatil25 <rajvpatil002@gmail.com>
2023-07-19 09:47:25 +0530

    update gitignore

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -5,7 +5,6 @@ import 'package:notesapp/otppage.dart';
 import 'backgroundtheme.dart';
 
 class LoginPage extends StatefulWidget {
-
   LoginPage({super.key});
 
   @override
@@ -36,31 +35,40 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             Positioned(
-              top: (Offset(MediaQuery.of(context).size.width * 0.5,0) - Offset(MediaQuery.of(context).size.width * 0.5, MediaQuery.of(context).size.height * 0.3)).distance
-                  - (MediaQuery.of(context).size.height * 0.1),
-                      // - (MediaQuery.of(context).size.height * 0.3),
+              top: (Offset(MediaQuery.of(context).size.width * 0.5, 0) -
+                          Offset(MediaQuery.of(context).size.width * 0.5, MediaQuery.of(context).size.height * 0.3))
+                      .distance -
+                  (MediaQuery.of(context).size.height * 0.1),
+              // - (MediaQuery.of(context).size.height * 0.3),
               child: ClipRRect(
                   borderRadius: BorderRadius.circular(100),
                   child: Image.asset(
                     "assets/verify.gif",
-                    height: MediaQuery.of(context).size.height * 0.2,width: MediaQuery.of(context).size.height * 0.2,
-                  )
-              ),
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    width: MediaQuery.of(context).size.height * 0.2,
+                  )),
             ),
             Padding(
               padding: const EdgeInsets.all(40),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text("Mobile Number",style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold,),),
+                  const Text(
+                    "Mobile Number",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(20.0),
                     child: Form(
                       key: input,
                       child: TextFormField(
-                        style: const TextStyle(letterSpacing: 4,fontFamily: "Roboto"),
-                        validator: (value){
-                          if( value == null || value.isEmpty){
+                        style: const TextStyle(letterSpacing: 4, fontFamily: "Roboto"),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
                             return "Enter mobile number.";
                           }
                           return null;
@@ -69,16 +77,9 @@ class _LoginPageState extends State<LoginPage> {
                         autofocus: true,
                         controller: phone,
                         decoration: InputDecoration(
-                          errorStyle: const TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 2
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            gapPadding: 20
-                          ),
+                          errorStyle:
+                              const TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold, letterSpacing: 2),
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(30), gapPadding: 20),
                           hintText: ("+91"),
                           filled: true,
                           fillColor: Colors.white,
@@ -88,44 +89,47 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   FloatingActionButton.extended(
                     backgroundColor: Colors.white,
-                    icon: isclicked?const Center(child: CircularProgressIndicator(color: Colors.cyan,)) : const Icon(Icons.password_outlined,color: Colors.black),
-                    onPressed: isclicked?() {}:() async {
-                      if(input.currentState!.validate()){
-                        setState((){
-                          isclicked = true;
-                        });
-                        await FirebaseAuth.instance.verifyPhoneNumber(
-                            phoneNumber: phone.text.contains("+91")
-                                ? phone.text
-                                : '+91${phone.text}',
-                            verificationFailed: (FirebaseAuthException error) {
-                              print(error);
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                      content: Text("Incorrect number.${error.message.toString()}")));
-                            },
-                            verificationCompleted:
-                                (PhoneAuthCredential phoneAuthCredential) {
-                              print('ok verify');
-                            },
-                            codeSent: (String verificationId,
-                                int? forceResendingToken) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(content: Text("OTP sent.")));
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (_) => OTPPage(
-                                        verifyID: verificationId,
-                                      )));
-                            },
-                            codeAutoRetrievalTimeout:
-                                (String verificationId) {},
-                            timeout: const Duration(seconds: 10));
-                        setState((){
-                          isclicked = false;
-                        });
-                      }
-                    },
-                    label: const Text("Get OTP",style: TextStyle(color: Colors.black),),
+                    icon: isclicked
+                        ? const Center(
+                            child: CircularProgressIndicator(
+                            color: Colors.cyan,
+                          ))
+                        : const Icon(Icons.password_outlined, color: Colors.black),
+                    onPressed: isclicked
+                        ? () {}
+                        : () async {
+                            if (input.currentState!.validate()) {
+                              setState(() {
+                                isclicked = true;
+                              });
+                              await FirebaseAuth.instance.verifyPhoneNumber(
+                                  phoneNumber: phone.text.contains("+91") ? phone.text : '+91${phone.text}',
+                                  verificationFailed: (FirebaseAuthException error) {
+                                    print(error);
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(content: Text("Incorrect number.${error.message.toString()}")));
+                                  },
+                                  verificationCompleted: (PhoneAuthCredential phoneAuthCredential) {
+                                    print('ok verify');
+                                  },
+                                  codeSent: (String verificationId, int? forceResendingToken) {
+                                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("OTP sent.")));
+                                    Navigator.of(context).push(MaterialPageRoute(
+                                        builder: (_) => OTPPage(
+                                              verifyID: verificationId,
+                                            )));
+                                  },
+                                  codeAutoRetrievalTimeout: (String verificationId) {},
+                                  timeout: const Duration(seconds: 10));
+                              setState(() {
+                                isclicked = false;
+                              });
+                            }
+                          },
+                    label: const Text(
+                      "Get OTP",
+                      style: TextStyle(color: Colors.black),
+                    ),
                     // shape: const Icon(Icons.arrow_forward),
                   ),
                 ],
